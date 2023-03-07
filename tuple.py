8e1a719e453fc12bf6f98cd5ed1d05f50c6ae0c6
DavidHe <s991179@gmail.com>
2023-03-07 09:45:33 +0800

    string format test

--- tuple.py
+++ tuple.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 tpl = (1, 3, 'XD', '9', 123)
 print(tpl)
 print(tpl[1])
@@ -9,3 +11,12 @@ tuples = [('1', '2'),('1', '3'), ('2', '4'), ('3', '6')]
 print(tuples)
 for a,b in tuples:
     print(a,b)
+tuples.append(('4', '8'))
+print(tuples)
+print([a+b for a,b in tuples])
+print(reduce(lambda x,y: x+"{{{0}, {1}}}".format(y[0], y[1]), tuples, ''))
+
+x=1
+y=2
+x,y=y,x
+print(x, y)
