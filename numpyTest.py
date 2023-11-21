d0e0ce9031c0dcd06ac5649975c725750f61f838
Dav He <s991179@gmail.com>
2023-11-22 02:37:20 +0800

    feat: add numpy

---
+++ numpyTest.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Create an empty 2-dimensional array (matrix) filled with zeros
+empty_matrix = np.zeros((3, 3))  # Creates a 3x3 matrix filled with zeros
+
+# Display the empty matrix
+print(empty_matrix)
+
+matrix3 = np.zeros((3,3,3))
+print(matrix3)
