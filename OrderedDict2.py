9de0d58b6d1159ea95be1f271bb368aa5ce6b9db
Dav He <s991179@gmail.com>
2024-09-30 02:25:30 +0800

    get first and last item from orderDict

--- OrderedDict2.py
+++ OrderedDict2.py
@@ -4,3 +4,7 @@ scores = OrderedDict([(8, 80), (7, 70), (10, 100)])
 print(scores)
 scores[1] = 10
 print(scores)
+print(scores.items())
+
+print(next(iter(scores.items())))
+print(next(reversed(scores.items())))
